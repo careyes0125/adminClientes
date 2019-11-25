6ca43d30926f33536aabfc08526ed23ae6abac73
ALCB01 <ALCB01@host.docker.internal>
2019-11-25 11:03:59 -0500

    fuentes y jar

--- back/src/main/java/com/alianza/Api/controllers/clientController.java
+++ back/src/main/java/com/alianza/Api/controllers/clientController.java
@@ -1,8 +1,9 @@
 package com.alianza.Api.controllers;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.alianza.Api.dtos.clientDto;
 import com.alianza.Api.entities.client;
 import com.alianza.Api.services.ClientServiceImpl;
 
 
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/api")
 public class clientController {
@@ -29,14 +31,13 @@ public class clientController {
 	ModelMapper modelMapper = new ModelMapper();
 	
 	@GetMapping("/clientes")
-	public clientDto[] index() {
+	public  clientDto[] index() {
 		return modelMapper.map(clientService.findAll(),clientDto[].class);
 	}
 
 	@GetMapping("/clientes/{id}")
-	public clientDto show(@PathVariable String id) {
-		
-		return  modelMapper.map(this.clientService.findById(id), clientDto.class);
+	public ResponseEntity<clientDto> show(@PathVariable String id) {
+		return ResponseEntity.ok(modelMapper.map(this.clientService.findById(id), clientDto.class));
 	}
 
 	@PostMapping("/clientes")
